dd547e670834fbe6c9131698333da1f9a1514908
Vishal Patil <vishalpatil.imp@gmail.com>
2026-05-29 16:33:22 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        Understand:
+            - n is the number of stairs
+            - 1 or 2 steps at a time
+        Input:  n - number of stairs to climb
+        Output: Number of distinct ways to climb the top
+        Plan:
+            - Recursively, keep track of current sum
+            - take 1 and 2 every time since we've these two choices
+            - if sum == 5, add to the answer
+            - if sum > 5, return and do not add
+            - if sum < 5, take 1 and 2 recursively again.
+        """
+        # Base Case
+        if n <= 2:
+            return n
+        
+        # Recursive Statement
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2) 
