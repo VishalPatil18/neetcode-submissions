059f416865a128103068c55d09c0393ac4b4e580
Vishal Patil <vishalpatil.imp@gmail.com>
2026-06-15 20:01:26 -0400

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def getHeight(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        leftHeight = 1 + self.getHeight(root.left)
+        rightHeight = 1 + self.getHeight(root.right)
+        
+        if leftHeight == -1 or rightHeight == -1:
+            return -1
+        
+        if abs(leftHeight - rightHeight) <= 1:
+            return max(leftHeight, rightHeight)
+        
+        return -1
+        
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        """
+        Understand: find if subtree height difference is more than 1
+        Input: root node
+        Output: bool
+        Plan:
+            - find height of left subtree
+            - find height of right subtree
+            - compare and check if diff is more than 1
+                - if yes -> return false
+                - else -> return true
+        Base Case: if root is None
+        """
+        if not root:
+            return True
+
+        leftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+
+        if leftHeight == -1 or rightHeight == -1:
+            return False
+
+        if abs(leftHeight - rightHeight) > 1:
+            return False
+        
+        return True        
